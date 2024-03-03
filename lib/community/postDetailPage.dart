856577b3177e868f559af5e40e8f03de318a99f9
so3659 <so3659@naver.com>
2024-03-04 05:51:20 +0900

    Implement Comment textformfield on postDetailPage

--- lib/community/postDetailPage.dart
+++ lib/community/postDetailPage.dart
@@ -16,57 +16,95 @@ class PostDetailPageState extends State<PostDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
-            backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(0.0),
-              child: Container(
-                color: Colors.grey.shade200,
-                height: 1.0,
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                const SizedBox(
-                  height: 6,
-                  width: double.infinity,
+      body: Column(
+        children: [
+          Expanded(
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  pinned: true,
+                  title: Text(
+                    'Main',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge!
+                        .copyWith(color: Colors.black87),
+                  ),
+                  iconTheme:
+                      IconThemeData(color: Theme.of(context).primaryColor),
+                  backgroundColor:
+                      Theme.of(context).appBarTheme.backgroundColor,
+                  bottom: PreferredSize(
+                    preferredSize: const Size.fromHeight(0.0),
+                    child: Container(
+                      color: Colors.grey.shade200,
+                      height: 1.0,
+                    ),
+                  ),
                 ),
-                widget.post.photoUrls != null
-                    ? FeedDetailPageBody(
-                        uid: widget.post.uid,
-                        content: widget.post.content,
-                        photoUrls: widget.post.photoUrls,
-                        dateTime: widget.post.dateTime,
-                        documentId: widget.post.documentId,
-                        currentUserId: widget.post.currentUserId,
-                      )
-                    : FeedDetailPageBody(
-                        uid: widget.post.uid,
-                        content: widget.post.content,
-                        dateTime: widget.post.dateTime,
-                        documentId: widget.post.documentId,
-                        currentUserId: widget.post.currentUserId,
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      const SizedBox(
+                        height: 6,
+                        width: double.infinity,
                       ),
-                Container(
-                  height: 6,
-                  width: double.infinity,
-                  color: TwitterColor.mystic,
-                )
+                      widget.post.photoUrls != null
+                          ? FeedDetailPageBody(
+                              uid: widget.post.uid,
+                              content: widget.post.content,
+                              photoUrls: widget.post.photoUrls,
+                              dateTime: widget.post.dateTime,
+                              documentId: widget.post.documentId,
+                              currentUserId: widget.post.currentUserId,
+                            )
+                          : FeedDetailPageBody(
+                              uid: widget.post.uid,
+                              content: widget.post.content,
+                              dateTime: widget.post.dateTime,
+                              documentId: widget.post.documentId,
+                              currentUserId: widget.post.currentUserId,
+                            ),
+                      Container(
+                        height: 6,
+                        width: double.infinity,
+                        color: TwitterColor.mystic,
+                      )
+                    ],
+                  ),
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                      [const Text('dfa'), const Text('dfa'), const Center()]),
+                ),
               ],
             ),
           ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              //!Removed container
-              const Center()
-            ]),
-          )
+          Container(
+            alignment: Alignment.bottomCenter,
+            margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: "댓글을 입력하세요",
+                hintStyle: const TextStyle(color: Colors.grey),
+                contentPadding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    gapPadding: 10,
+                    borderSide: BorderSide(color: Colors.lightBlue[200]!)),
+                suffixIcon: IconButton(
+                  onPressed: () async {},
+                  icon: const Icon(Icons.send),
+                  color: Colors.lightBlue[200],
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.lightBlue[200]!,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
