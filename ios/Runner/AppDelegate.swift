74560322407af5c2e72860ffa0e14297fee07ec2
so3659 <so3659@naver.com>
2024-04-16 08:00:19 +0900

    Solve iOS FCM Err#6

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,9 +11,35 @@ import FirebaseMessaging
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+	// GoogleSerice-info.plist에 File I/O 하는 기능
+    FirebaseApp.configure()
+    
+    // 알림 delegate 설정
+    UNUserNotificationCenter.current().delegate = self
+    
+    // 알림 허용 확인
+    UNUserNotificationCenter.current().requestAuthorization(
+        options: [.alert, .sound, .badge, .providesAppNotificationSettings]) { didAllow, error  in
+        print("Notification Authorization : \(didAllow)")
+    }
+    
+    // 원격 알림에 앱 등록
+    application.registerForRemoteNotifications()
+    
+    
+    // Messaging delegate 설정
+    Messaging.messaging().delegate = self
+    
+    
+    return true
+  }
+  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
 
    Messaging.messaging().apnsToken = deviceToken
-   super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
  }
+ func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    print(error)
+  }
 }
