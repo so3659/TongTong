857fd17342b57ace0e92613333801b67067ef12a
so3659 <so3659@naver.com>
2024-04-16 08:19:05 +0900

    Solve iOS FCM Err#7

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,45 +1,36 @@
 import UIKit
 import Flutter
 import FirebaseMessaging
+import Firebase
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate { // MessagingDelegate 추가
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure() // Firebase 초기화
 
-	// GoogleSerice-info.plist에 File I/O 하는 기능
-    FirebaseApp.configure()
-    
-    // 알림 delegate 설정
     UNUserNotificationCenter.current().delegate = self
     
-    // 알림 허용 확인
     UNUserNotificationCenter.current().requestAuthorization(
         options: [.alert, .sound, .badge, .providesAppNotificationSettings]) { didAllow, error  in
         print("Notification Authorization : \(didAllow)")
     }
     
-    // 원격 알림에 앱 등록
     application.registerForRemoteNotifications()
     
-    
-    // Messaging delegate 설정
     Messaging.messaging().delegate = self
-    
     
     return true
   }
-  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
 
-   Messaging.messaging().apnsToken = deviceToken
- }
- func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Messaging.messaging().apnsToken = deviceToken
+  }
+
+  override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
     print(error)
   }
 }
