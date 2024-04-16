954cea94b1538cd5081dfc609334bc8de3429b68
so3659 <so3659@naver.com>
2024-04-16 10:39:52 +0900

    Solve iOS FCM Err#13

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,4 +11,7 @@ import FirebaseMessaging
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+  if #available(iOS 10.0, *) {
+    UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+  }
 }
