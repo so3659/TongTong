db02e3df6127e2b98e2d16702cb3e1868c9894c8
so3659 <so3659@naver.com>
2024-04-16 10:28:57 +0900

    Solve iOS FCM Err#11

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,38 +1,14 @@
 import UIKit
 import Flutter
 import FirebaseMessaging
-import Firebase
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate { // MessagingDelegate 추가
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    if FirebaseApp.app() == nil {
-    FirebaseApp.configure()
-    }
-
-    UNUserNotificationCenter.current().delegate = self
-    
-    UNUserNotificationCenter.current().requestAuthorization(
-        options: [.alert, .sound, .badge, .providesAppNotificationSettings]) { didAllow, error  in
-        print("Notification Authorization : \(didAllow)")
-    }
-    
-    application.registerForRemoteNotifications()
-    
-    Messaging.messaging().delegate = self
-    
-    return true
-  }
-
-  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    Messaging.messaging().apnsToken = deviceToken
-  }
-
-  override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-    print(error)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
